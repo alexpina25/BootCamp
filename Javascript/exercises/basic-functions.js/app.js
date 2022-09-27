f9e80a508cf560b109cc8598ae11f5dddef5d36c
alexpina <alexpina25@gmail.com>
2022-09-27 16:37:17 +0200

    feat: Iteración #2: Buscar la palabra más larga

--- Javascript/exercises/basic-functions.js/app.js
+++ Javascript/exercises/basic-functions.js/app.js
@@ -13,4 +13,29 @@ function sum(numberOne, numberTwo) {
 }
 console.log(sum(5, 10));
 
+/************************************
+ * Iteración #2: Buscar la palabra más larga *
+ ***********************************/
 
+//Completa la función que tomando un array de strings como argumento devuelva el más largo,
+// en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+
+const avengers = [
+  "Hulk",
+  "Thor",
+  "IronMan",
+  "Captain A.",
+  "Spiderman",
+  "Captain M.",
+];
+function findLongestWord(array) {
+  let longestWord = "";
+
+  for (const iterator of array) {
+    if (iterator.length > longestWord.length) {
+      longestWord = iterator;
+    }
+  }
+  return longestWord;
+}
+console.log(findLongestWord(avengers));
