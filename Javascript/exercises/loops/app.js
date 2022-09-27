10bcc7aa36c85439122a12536561cb7602ca1fe6
alexpina <alexpina25@gmail.com>
2022-09-27 20:01:48 +0200

    feat: Iteración #3: Probando For...of

--- Javascript/exercises/loops/app.js
+++ Javascript/exercises/loops/app.js
@@ -43,9 +43,26 @@ function approveAlumn(array) {
     ) {
       iterator.isApproved = true;
     } else {
-        iterator.isApproved = false;
+      iterator.isApproved = false;
     }
   }
   console.log(array);
 }
 approveAlumn(alumns);
+
+/***************************
+ * Iteración #3: Probando For...of *
+ ***************************/
+// Usa un bucle forof para recorrer todos los destinos del array. Imprime en un console.log sus valores.
+const placesToTravel = [
+  "Japon",
+  "Venecia",
+  "Murcia",
+  "Santander",
+  "Filipinas",
+  "Madagascar",
+];
+
+for (const iterator of placesToTravel) {
+  console.log(iterator);
+}
