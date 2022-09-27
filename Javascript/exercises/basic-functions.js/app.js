b654542763218b4d93fad8d7ad9bf038150d71ed
alexpina <alexpina25@gmail.com>
2022-09-27 16:31:37 +0200

    feat: Iteración #1: Buscar el máximo

---
+++ Javascript/exercises/basic-functions.js/app.js
@@ -0,0 +1,16 @@
+/************************************
+ * Iteración #1: Buscar el máximo *
+ ***********************************/
+
+// Completa la función que tomando dos números como argumento devuelva el más alto.
+
+function sum(numberOne, numberTwo) {
+  if (numberOne > numberTwo) {
+    return numberOne;
+  } else {
+    return numberTwo;
+  }
+}
+console.log(sum(5, 10));
+
+
