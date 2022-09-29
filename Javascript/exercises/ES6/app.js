f4cdd21a2e42f3c3a24bda48943a639dfa38ebc9
alexpina <alexpina25@gmail.com>
2022-09-29 20:03:17 +0200

    feat: Iteración #7: Reduce

--- Javascript/exercises/ES6/app.js
+++ Javascript/exercises/ES6/app.js
@@ -257,3 +257,44 @@ console.log(porom);
 
 const alienMutation = { ...cucu, mutation: porom };
 console.log(alienMutation);
+
+/************************************
+ * Iteración #7: Reduce *
+ ***********************************/
+/*  7.1 Dado el siguiente array, haz una suma de todos las notas de los examenes de 
+ los alumnos usando la función .reduce(). */
+
+const exams = [
+  { name: "Yuyu Cabeza Crack", score: 5 },
+  { name: "Maria Aranda Jimenez", score: 1 },
+  { name: "Cristóbal Martínez Lorenzo", score: 6 },
+  { name: "Mercedez Regrera Brito", score: 7 },
+  { name: "Pamela Anderson", score: 3 },
+  { name: "Enrique Perez Lijó", score: 6 },
+  { name: "Pedro Benitez Pacheco", score: 8 },
+  { name: "Ayumi Hamasaki", score: 4 },
+  { name: "Robert Kiyosaki", score: 2 },
+  { name: "Keanu Reeves", score: 10 },
+];
+
+const examNotes = exams.reduce((acc, { score }) => acc + score, 0);
+console.log(examNotes);
+
+/*  7.2 Dado el mismo array, haz una suma de todos las notas de los examenes de los 
+ alumnos que esten aprobados usando la función .reduce(). */
+let total = 0;
+const approvedTotal = exams.reduce((acc, { score }) => {
+  if (score > 5) {
+    total = acc + score;
+  }
+  return total;
+}, 0);
+console.log(approvedTotal);
+
+/*  7.3 Dado el mismo array, haz la media de las notas de todos los examenes .reduce(). */
+
+const averageNotes = exams.reduce((acc, { score }) => {
+  return acc + score;
+}, 0);
+const average = averageNotes / exams.length;
+console.log(average);
