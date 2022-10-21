e6304272b754b95495e83cbf3beea59473306d12
alexpina <alexpina25@gmail.com>
2022-10-21 13:26:58 +0200

    feat: extra - exercise-4

---
+++ Javascript/extra-exercises/extra/exercise-4.js
@@ -0,0 +1,14 @@
+/* Segun la siguiente lista, ordena los elementos en base a la propiedad .order. */
+
+const list = [
+  { name: "Mc Aitana", gender: "pop", order: 3 },
+  { name: "Tote Queen", gender: "rap", order: 2 },
+  { name: "Good Bunny", gender: "reggeaton", order: 4 },
+  { name: "El sueño de Neo", gender: "pop", order: 1 },
+];
+
+list.sort(function (a, b) {
+  return a.order - b.order;
+});
+
+console.log(list);
