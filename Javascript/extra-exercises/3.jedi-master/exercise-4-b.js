413583a9cc723e8c96f3a3c0d0a77a9cfe966dcf
alexpina <alexpina25@gmail.com>
2022-10-21 12:18:19 +0200

    feat: jedi-master - exercise-4-b

---
+++ Javascript/extra-exercises/3.jedi-master/exercise-4-b.js
@@ -0,0 +1,54 @@
+const places = [
+  {
+    title: "Random title",
+    imgUrl:
+      "https://images.unsplash.com/photo-1506466010722-395aa2bef877?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bG9yZCUyMG9mJTIwdGhlJTIwcmluZ3N8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    title: "Random title",
+    imgUrl:
+      "https://images.unsplash.com/photo-1570610155223-66279ba81b41?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bG9yZCUyMG9mJTIwdGhlJTIwcmluZ3N8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    title: "Random title",
+    imgUrl:
+      "https://images.unsplash.com/photo-1570888233388-35d777042d9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGxvcmQlMjBvZiUyMHRoZSUyMHJpbmdzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    title: "Random title",
+    imgUrl:
+      "https://images.unsplash.com/photo-1490440101319-4c8eb3880432?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fGxvcmQlMjBvZiUyMHRoZSUyMHJpbmdzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    title: "Random title",
+    imgUrl:
+      "https://images.unsplash.com/photo-1460453429228-912eec8be349?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fGxvcmQlMjBvZiUyMHRoZSUyMHJpbmdzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+];
+
+for (const place of places) {
+  let div = document.createElement("div");
+  let h4 = document.createElement("h4");
+  let img = document.createElement("img");
+
+  h4.innerHTML = place.title;
+  img.src = place.imgUrl;
+
+  div.appendChild(h4);
+  div.appendChild(img);
+  document.body.appendChild(div);
+}
+
+const btnRemove = document.querySelector("button");
+
+btnRemove.addEventListener("click", () => {
+  let elements = document.querySelectorAll("h4");
+  let lastElement = elements[elements.length - 1];
+
+  if (elements.length >= 1) {
+    lastElement.parentNode.remove(lastElement);
+    console.log(elements.length);
+  } else {
+    btnRemove.setAttribute("disabled", true);
+  }
+});
