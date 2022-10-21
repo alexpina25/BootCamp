9f34e5f11cd146b4f3bd0e98de89462b590886de
alexpina <alexpina25@gmail.com>
2022-10-21 11:39:15 +0200

    feat: exercise-6

---
+++ Javascript/extra-exercises/2.jedi-knight/exercise-6.js
@@ -0,0 +1,20 @@
+/* Crea una función llamada `swap` que reciba un array y dos parametros que sean indices del array.
+ La función deberá intercambiar la posición de los valores de los indices que hayamos enviado como parametro. Retorna el array resultante. */
+
+const fantasticFour = [
+  "La antorcha humana",
+  "Mr. Fantástico",
+  "La mujer invisible",
+  "La cosa",
+];
+
+const swap = (array, index1, index2) => {
+  const memory = array[index1];
+
+  array[index1] = array[index2];
+  array[index2] = memory;
+
+  return array;
+};
+
+console.log(swap(fantasticFour, 0, 3));
