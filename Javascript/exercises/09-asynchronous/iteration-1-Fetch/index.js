98013dd745843dd43b69db7243740e28d9891029
alexpina <alexpina25@gmail.com>
2022-10-24 19:59:56 +0200

    feat: iteration-1 - 2.1

--- Javascript/exercises/09-asynchronous/iteration-1-Fetch/index.js
+++ Javascript/exercises/09-asynchronous/iteration-1-Fetch/index.js
@@ -13,3 +13,28 @@ const getData = async () => {
 };
 
 getData();
+
+/* 2.1 Dado el siguiente javascript y html. Añade la funcionalidad necesaria usando 
+fetch() para hacer una consulta a la api cuando se haga click en el botón, 
+pasando como parametro de la api, el valor del input. */
+
+const baseUrl = "https://api.nationalize.io";
+
+const getData2 = async (name) => {
+  try {
+    const rawData = await fetch(`https://api.nationalize.io?name=${name}`);
+    const jsonData = await rawData.json();
+    console.log(jsonData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  console.log(input)
+  console.log(input.value)
+  getData2(input.value);
+});
